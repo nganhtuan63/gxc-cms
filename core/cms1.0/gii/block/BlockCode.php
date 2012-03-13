c4b1cfc02150671e2f2892f8a4b7fd050ac62d32
Nguyen Tuan Quyen <nguyen.tuan.quyen.it@gmail.com>
2012-03-14 02:07:42 +0700

    Optimize code: block generation

--- core/cms1.0/gii/block/BlockCode.php
+++ core/cms1.0/gii/block/BlockCode.php
@@ -7,7 +7,8 @@
 class BlockCode extends CCodeModel
 {
 	public $blockName;	// block name zone that will be filled by user before generating code
-	public $blockID; 
+	public $blockID;
+	public $blockClass; 
 	
 	public $baseClass = 'Object';
 	
@@ -27,21 +28,23 @@ class BlockCode extends CCodeModel
         ));
     }
     
-	/* Main function that generates the code for new block based on template files
+	/* Prepare for generating the code for new block based on template files
 	 * 
 	 * @see CCodeModel::prepare()
 	 */
 	public function prepare()
     {
-    	$blockID = $this->getBlockID($this->blockName);
-   	    $blockPath = COMMON_FOLDER.DIRECTORY_SEPARATOR.'front_blocks'.DIRECTORY_SEPARATOR. $blockID;
-   	    $blockClass = $this->getBlockClass($this->blockName);
+    	//get the block ID based on the block name. Ex: 'New Example' -> 'new_example'
+    	$this->blockID = GxcHelpers::encode($this->blockName, '_', false);
+   	    $blockPath = COMMON_FOLDER.DIRECTORY_SEPARATOR.'front_blocks'.DIRECTORY_SEPARATOR. $this->blockID;
+   	    //Get the block class based on the block name. Ex: 'New Example' -> 'NewExampleBlock'
+   	    $this->blockClass = GxcHelpers::encode($this->blockName).'Block';
 
    	    //Get the path of new code to be generated
         $iniPath = $blockPath.DIRECTORY_SEPARATOR.'info.ini';
-		$blockInputPath = $blockPath.DIRECTORY_SEPARATOR. $blockID.'_block_input.php';
-		$blockOutputPath = $blockPath.DIRECTORY_SEPARATOR.$blockID.'_block_output.php'; 
-		$blockClassPath = $blockPath.DIRECTORY_SEPARATOR.$blockClass. '.php';
+		$blockInputPath = $blockPath.DIRECTORY_SEPARATOR. $this->blockID.'_block_input.php';
+		$blockOutputPath = $blockPath.DIRECTORY_SEPARATOR.$this->blockID.'_block_output.php'; 
+		$blockClassPath = $blockPath.DIRECTORY_SEPARATOR.$this->blockClass. '.php';
 		
 		//Get the path of template files (in the folder templates)
         $iniFile = $this->render($this->templatepath.DIRECTORY_SEPARATOR.'info.ini');
@@ -54,39 +57,5 @@ class BlockCode extends CCodeModel
         $this->files[]=new CCodeFile($blockInputPath, $blockInputFile);
         $this->files[]=new CCodeFile($blockOutputPath, $blockOutputFile);
         $this->files[]=new CCodeFile($blockClassPath, $blockClassFile);
-    }
-    
-    /**
-     * This function returns the block id based on the block name. For example :
-     * Block name = 'New Example' => Block id = 'new_example'
-     * @param unknown_type $blockName
-     */
-    public function getBlockID($blockName)
-    {
-    	$blockID = null;
-    	if(!isset($this->blockID))
-    	{
-    		$blockID = preg_replace('/\s[\s]+/','_',$blockName);    // Strip off multiple spaces 
-    		$blockID = preg_replace('/[\s\W]+/','_',$blockID);    // Strip off spaces and non-alpha-numeric 
-    		$blockID = preg_replace('/^[\_]+/','',$blockID); // Strip off the starting '_'
-    		$blockID = preg_replace('/[\_]+$/','',$blockID); // // Strip off the ending '_'
-    		$blockID = strtolower($blockID); 
-    		$this->blockID = $blockID;
-    	}
-    	return $this->blockID;
-    }
-    
-    /**
-     * This function returns the block class name which is defined in the BlockCode.php. For ex:
-     * Block name = 'New Example' => Block class name = 'NewExamplelBlock'
-     * @param unknown_type $blockName
-     * @return string
-     */
-    public function getBlockClass($blockName)
-    {
-    	$blockClass = ucwords(strtolower($blockName));
-    	$blockClass = preg_replace('/\s[\s]+/','',$blockClass);    // Strip off multiple spaces 
-    	$blockClass = preg_replace('/[\s\W]+/','',$blockClass);    // Strip off spaces and non-alpha-numeric
-    	return $blockClass.'Block';
     }
 }
