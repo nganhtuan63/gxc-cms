6005bac09db4599bd20586b6a5dca9d2c02c72c0
Tuan Nguyen <nganhtuan63@gmail.com>
2012-03-13 22:08:07 +0700

    Edit caching

--- core/cms1.0/widgets/views/caching/caching_widget.php
+++ core/cms1.0/widgets/views/caching/caching_widget.php
@@ -3,5 +3,5 @@
 <a class="button" href="<?php echo bu();?>/becaching/clear?cache_id=backend_assets"><?php echo t('Clear BACKEND Assets'); ?></a>
 <a class="button" href="<?php echo bu();?>/becaching/clear?cache_id=backend_cache"><?php echo t('Clear BACKEND Cache'); ?></a>
 <a class="button" href="<?php echo bu();?>/becaching/clear?cache_id=frontend_assets"><?php echo t('Clear FRONTEND Assets'); ?></a>
-<a class="button" href="<?php echo bu();?>/becaching/clear?cache_id=backend_cache"><?php echo t('Clear FRONTEND Cache'); ?></a>
+<a class="button" href="<?php echo bu();?>/becaching/clear?cache_id=frontend_cache"><?php echo t('Clear FRONTEND Cache'); ?></a>
 </div>
