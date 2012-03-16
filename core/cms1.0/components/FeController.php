f2132ad53a05279d435ee54ebc5edb34a3b408b8
Tuan Nguyen <nganhtuan63@gmail.com>
2012-03-16 17:54:34 +0700

    Edit Frontend Controller for Title of Object Detail modification

--- core/cms1.0/components/FeController.php
+++ core/cms1.0/components/FeController.php
@@ -10,7 +10,11 @@
  */
 
 class FeController extends RController
-{	
+{
+
+		public $description;
+		public $keywords;			
+		public $change_title=false;
         
         public function __construct($id,$module=null)
 		{
@@ -51,14 +55,34 @@ class FeController extends RController
                 'condition'=>'slug=:paramId',
                 'params'=>array(':paramId'=>$slug))); 								
             if($page){
-                $this->layout='main';	    
+                $this->layout='main';
+				$this->pageTitle=$page->title;
+				$this->description=$page->description;
+				$this->keywords=$page->keywords;	    
                 //depend on the layout of the page, use the corresponding file to render                
-                    $this->render('common.front_layouts.'.$page->layout.'.'.$page->display_type,array('page'=>$page));                                
+                $this->render('common.front_layouts.'.$page->layout.'.'.$page->display_type,array('page'=>$page));                                
             } else {            	  
                   throw new CHttpException('404',t('Oops! Page not found!'));
             }
         }
-        
+       		
+			
+		public function afterRender($view,&$output)
+	    {
+	    		   	            
+			Yii::app()->clientScript->registerMetaTag($this->description, 'description');
+			Yii::app()->clientScript->registerMetaTag($this->keywords, 'keywords');
+						
+			//Check if change Title, we will replace content in <title> with new Title
+			if($this->change_title){				
+				$output=replaceTags('<title>', '</title>', $this->pageTitle, $output);								
+			}	
+			
+				 
+	    }
+		
+		
+	   
         public function error(){
             if($error=Yii::app()->errorHandler->error)
 		    {
@@ -69,5 +93,6 @@ class FeController extends RController
 		    }
         }
        
+	
 
 }
